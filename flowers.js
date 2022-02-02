b2770b74fe769b6584363034608ec06a6fcbf0b3
sseals1 <email@example.com>
2022-02-01 21:03:47 -0600

    finished flowers project

--- flowers.js
+++ flowers.js
@@ -1,22 +1,26 @@
 const flowers = [
-    
-{
-id: 1,
-color: "white",
-species: "White Rose",
-price: 0.90
-},
-
-{    
-id: 2,
-color: "red",
-species: "Red Tulip",
-price: 1.10
-}
-
+    {
+        id: 1,
+        color: "white Rose",
+        species: "rose",
+        price: 0.90
+    },
+    {
+        id: 2,
+        color: "Red Tulip",
+        species: "tulip",
+        price: 1.10
+    }
 ]
 
 const addFlower = (flowerObject) => {
+    const lastIndex = flowers.length - 1
+    const currentLastToy = flowers[lastIndex]
+    const maxId = currentLastToy.id
+    const idForNewFlower = maxId + 1
+
+    flowerObject.id = idForNewFlower
+    flowers.push(flowerObject)
     /*
         This function should add an `id` property
         to the object that was sent to the function.
@@ -28,12 +32,7 @@ const addFlower = (flowerObject) => {
         object, the object should then be put into the
         `flowers` array.
     */
-    const lastIndexOfArray = flowers.length - 1
-    const lastObjectInArray = flowers[lastIndexOfArray]
-    const lastIdOfArray = lastObjectInArray.id
-    const newValueId = lastIdOfArray + 1
-    flowerObject.id = newValueId
-    flowers.push(flowerObject)
+
 
 }
 
@@ -41,6 +40,11 @@ const addFlower = (flowerObject) => {
 const findExpensiveFlowers = () => {
     const expensiveFlowers = []  // Do not change this code
 
+        for (const flower of flowers) {
+            if (flower.price >= 1.00) {
+                expensiveFlowers.push(flower)
+            }
+        }
 
     /*
         Write a for..of loop that iterate the array
@@ -48,11 +52,7 @@ const findExpensiveFlowers = () => {
         greater than or equal to 1.00, it should be
         added to the `expensiveFlowers` array.
     */
-for (const flower of flowers){
-    if (flower.price > 1.00){
-        expensiveFlowers.push(flower)
-    }
-}
+
 
 
 
